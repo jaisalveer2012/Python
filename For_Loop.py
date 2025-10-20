f156f75f563baa9a18334254c9ff956cab71d8fb
jaisalveer2012 <jaisalveer2012@gmail.com>
2025-10-20 22:39:55 +0530

    nested loop

---
+++ For_Loop.py
@@ -0,0 +1,9 @@
+my_list = ["R1", "R2", "R3", "CORESWITCH", "DISTROSWITCH"]    # my_list is list of devices
+
+for device in my_list:            # device is any variable you can choose
+    print(f"I have device {device} in my network")     #'f' format string
+
+test_string = "This is a just test string to test for loops"
+
+for character in test_string:
+    print(character)
---
+++ Nested_Loop.py
@@ -0,0 +1,6 @@
+device_list = ["R1", "R2", "R3"]
+interface_list = ["interface g0/0", "interface g0/1", "interface g0/2"]
+for device in device_list:
+    for interface in interface_list:
+        print (f"{interface}\n description connected to {device}")
+    
