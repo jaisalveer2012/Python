ccbc20720ca723a75fba7d066f104290dcfba661
jaisalveer2012 <jaisalveer2012@gmail.com>
2025-10-25 19:24:57 +0530

    while-loop

---
+++ While-Loop.py
@@ -0,0 +1,19 @@
+# num = 5
+
+# while num < 10:
+#     print (f"The number {num} is less than 10")
+#     num +=1
+
+# num = input ("Enter the number : ")
+# num_1 = int(num)
+# while num_1 < 100:
+#     print (f"The number {num_1} is less than 100")
+#     num_1 +=1
+
+num = input ("Enter the number : ")
+num_1 = int(num)
+while num_1 < 100:
+    print (f"The number {num_1} is less than 100")
+    if num_1 == 75:
+        break
+    num_1 +=1
