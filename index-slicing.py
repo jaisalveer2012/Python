ef7d2750f5a6a1146db2d283b77cadcc7a83d700
jaisalveer2012 <jaisalveer2012@gmail.com>
2025-10-26 17:55:07 +0530

    Slicing

---
+++ index-slicing.py
@@ -0,0 +1,15 @@
+new_vlans = ["Vlan5", "Vlan10","Vlan15","Vlan20","Vlan25","Vlan30","Vlan35","Vlan40"]
+output = new_vlans[0:4]
+print(output)
+
+new_vlans = ["Vlan5", "Vlan10","Vlan15","Vlan20","Vlan25","Vlan30","Vlan35","Vlan40"]
+output = new_vlans[:3]
+print(output)
+
+new_vlans = ["Vlan5", "Vlan10","Vlan15","Vlan20","Vlan25","Vlan30","Vlan35","Vlan40"]
+output = new_vlans[2:]
+print(output)
+
+new_vlans = ["Vlan5", "Vlan10","Vlan15","Vlan20","Vlan25","Vlan30","Vlan35","Vlan40"]
+output = new_vlans[1:8:1]
+print(output)
