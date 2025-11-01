c37836bfb4da536d382d526b819d00fa56070b9e
jaisalveer2012 <jaisalveer2012@gmail.com>
2025-11-01 12:02:32 +0530

    dict

--- dict.py
+++ dict.py
@@ -1,5 +1,8 @@
-# my_dict = {"name" : "John", "age" : 43 , "city" : "UK" , "name" : "Rohit"}
+my_dict = {"name" : "John", "age" : 43 , "city" : "UK" , "name" : "Rohit"}
 # print(my_dict)    #Due to duplicate key as "name", Rohit will override John
 
-my_dict1 = [{"name" : "John", "age" : 43 , "city" : "UK" , "name" : "Rohit"}, {"name" : "John", "age" : 43}]
-print(type(my_dict1)) # list with dict
+# my_dict1 = [{"name" : "John", "age" : 43 , "city" : "UK" , "name" : "Rohit"}, {"name" : "John", "age" : 43}]
+# print(type(my_dict1)) # list with dict
+
+del my_dict["name"]
+print(my_dict)
