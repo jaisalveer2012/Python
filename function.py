cbf503294874ff3f799818d5b2163c792c316984
jaisalveer2012 <jaisalveer2012@gmail.com>
2025-11-02 23:00:07 +0530

    function

---
+++ function.py
@@ -0,0 +1,23 @@
+def my_function ():
+    print("This is a custom function")
+
+my_function ()
+
+def adder(a,b):
+    print(a + b)
+
+adder(2 , 8)
+
+adder("john", "india")
+
+def email(username, provider):
+    """
+    This function takes 2 arguments. First pass in a username
+    Second pass in your email provider
+    """
+    print(f"your email is {username}@{provider}.com")
+
+email("john", "cbt")
+
+
+
